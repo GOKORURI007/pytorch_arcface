09065eb43e7336540683c32dc72b92c9b717de77
GokouRuri <40916324+GOUKOU007@users.noreply.github.com>
2021-11-02 13:20:39 +0800

    Update arcface.py
    bug fix

--- arcface.py
+++ arcface.py
@@ -29,25 +29,25 @@ class ArcFace(nn.Module):
 
         nn.init.xavier_uniform_(self.weight)
 
-    def forward1(self, embedding: torch.Tensor, ground_truth):
+        def forward(self, embedding: torch.Tensor, ground_truth):
         """
-        This implementation is from https://github.com/deepinsight/insightface, which takes
-        53.55467200570274 ms for every 100 times of input (50, 512) and output (50, 10000) on 2080Ti.
+        This Implementation is from https://github.com/ronghuaiyang/arcface-pytorch, which takes
+        54.804054962005466 ms for every 100 times of input (50, 512) and output (50, 10000) on 2080Ti.
         """
         # --------------------------- cos(theta) & phi(theta) ---------------------------
-        cosine = F.linear(F.normalize(embedding), F.normalize(self.weight)).clamp(-1 + 1e-7, 1 - 1e-7)
-        sine = torch.sqrt((1.0 - torch.pow(cosine, 2)).clamp(0 + 1e-7, 1 - 1e-7))
-        phi = cosine * self.cos_m - sine * self.sin_m
+        cos_theta = F.linear(F.normalize(embedding), F.normalize(self.weight)).clamp(-1 + 1e-7, 1 - 1e-7)
+        sin_theta = torch.sqrt((1.0 - torch.pow(cos_theta, 2)).clamp(-1 + 1e-7, 1 - 1e-7))
+        phi = cos_theta * self.cos_m - sin_theta * self.sin_m
         if self.easy_margin:
-            phi = torch.where(cosine > 0, phi, cosine)
+            phi = torch.where(cos_theta > 0, phi, cos_theta)
         else:
-            phi = torch.where(cosine > self.th, phi, cosine - self.mm)
+            phi = torch.where(cos_theta > self.th, phi, cos_theta - self.mm)
         # --------------------------- convert label to one-hot ---------------------------
-        one_hot = torch.zeros(cosine.size(), device='cuda')
+        one_hot = torch.zeros(cos_theta.size(), device='cuda')
         one_hot.scatter_(1, ground_truth.view(-1, 1).long(), 1)
         # -------------torch.where(out_i = {x_i if condition_i else y_i) -------------
         output = (one_hot * phi) + (
-                (1.0 - one_hot) * cosine)  # you can use torch.where if your torch.__version__ is 0.4
+                (1.0 - one_hot) * cos_theta)  # you can use torch.where if your torch.__version__ is 0.4
         output *= self.scale
 
         loss = self.ce(output, ground_truth)
@@ -55,42 +55,45 @@ class ArcFace(nn.Module):
 
     def forward2(self, embedding: torch.Tensor, ground_truth):
         """
-        This implementation is from https://github.com/deepinsight/insightface, which takes
-        75.16696600941941 ms for every 100 times of input (50, 512) and output (50, 10000) on 2080Ti.
+        This Implementation is from https://github.com/deepinsight/insightface, which takes
+        66.45489303627983 ms for every 100 times of input (50, 512) and output (50, 10000) on 2080 Ti.
         Please noted that, different with forward1&3, this implementation ignore the samples that
-        caused \theta + m > \pi to happen.
+        caused \theta + m > \pi to happen if easy_margin is False. And if easy_margin is True,
+        it will do nothing even if \theta + m > \pi.
         """
         embedding = F.normalize(embedding)
         w = F.normalize(self.weight)
-        cosine = F.linear(embedding, w)
-        cosine = torch.clamp(cosine, -1.0 + 1e-7, 1.0 - 1e-7)
-        mask = torch.gather(cosine, 1, ground_truth.view(-1, 1)).view(-1)
-        mask = torch.where(mask.acos_() + self.margin > math.pi, 0, 1)
+        cos_theta = F.linear(embedding, w).clamp(-1.0 + 1e-7, 1.0 - 1e-7)
+        if self.easy_margin:
+            mask = torch.ones_like(ground_truth)
+        else:
+            mask = torch.gather(cos_theta, 1, ground_truth.view(-1, 1)).view(-1)
+            mask = torch.where(mask.acos_() + self.margin > math.pi, 0, 1)
         mask = torch.where(mask != 0)[0]
-        m_hot = torch.zeros(mask.shape[0], cosine.shape[1], device=cosine.device)
+        m_hot = torch.zeros(mask.shape[0], cos_theta.shape[1], device=cos_theta.device)
         m_hot.scatter_(1, ground_truth[mask, None], self.margin)
-        cosine.acos_()
-        cosine += m_hot
-        cosine.cos_().mul_(self.scale)
-        loss = self.ce(cosine[mask], ground_truth[mask])
+        theta = cos_theta.acos()
+        output = (theta + m_hot).cos()
+        output.mul_(self.scale)
+        loss = self.ce(output[mask], ground_truth[mask])
         return loss
 
     def forward3(self, embedding: torch.Tensor, ground_truth):
         """
-        This implementation is modified from forward1, which takes
-        54.52143200091086 ms for every 100 times of input (50, 512) and output (50, 10000) on 2080Ti.
+        This Implementation is modified from forward1, which takes
+        52.49644996365532 ms for every 100 times of input (50, 512) and output (50, 10000) on 2080 Ti.
         """
-        cosine = F.linear(F.normalize(embedding), F.normalize(self.weight)).clamp(-1 + 1e-7, 1 - 1e-7)
-        pos = torch.gather(cosine, 1, ground_truth.view(-1, 1))
-        sine = torch.sqrt((1.0 - torch.pow(cosine, 2)).clamp(0 + 1e-7, 1 - 1e-7))
-        phi = pos * self.cos_m - sine * self.sin_m
+        cos_theta = F.linear(F.normalize(embedding), F.normalize(self.weight)).clamp(-1 + 1e-7, 1 - 1e-7)
+        pos = torch.gather(cos_theta, 1, ground_truth.view(-1, 1))
+        sin_theta = torch.sqrt((1.0 - torch.pow(pos, 2)).clamp(-1 + 1e-7, 1 - 1e-7))
+        phi = pos * self.cos_m - sin_theta * self.sin_m
         if self.easy_margin:
-            phi = torch.where(pos > 0, phi, cosine)
+            phi = torch.where(pos > 0, phi, pos)
         else:
-            phi = torch.where(pos > self.th, phi, cosine - self.mm)
-        one_hot = torch.zeros(cosine.size(), device='cuda')
-        one_hot.scatter_(1, ground_truth.view(-1, 1).long(), phi)
-        cosine += one_hot
-        cosine *= self.scale
-        loss = self.ce(cosine, ground_truth)
+            phi = torch.where(pos > self.th, phi, pos - self.mm)
+        # one_hot = torch.zeros(cos_theta.size(), device='cuda')
+        output = torch.scatter(cos_theta, 1, ground_truth.view(-1, 1).long(), phi)
+        # output = cos_theta + one_hot
+        output *= self.scale
+        loss = self.ce(output, ground_truth)
         return loss
