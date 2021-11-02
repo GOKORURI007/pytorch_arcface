8b5f3c78520e515c033782c36dfb096fa650319d
GokouRuri <40916324+GOUKOU007@users.noreply.github.com>
2021-11-02 13:23:37 +0800

    Update speed_test.py
    make it easy to use

--- speed_test.py
+++ speed_test.py
@@ -4,29 +4,48 @@ import torch
 
 from arcface import ArcFace
 
-label = torch.randint(0, 999, (50,)).to("cuda")
-embed = torch.randn((50, 512)).to("cuda") - 2
-model = ArcFace(512, 10000, 30, 0.2, True).to("cuda")
+device = "cuda" if torch.cuda.is_available() else "cpu"
+label = torch.randint(0, 9999, (50,)).to(device)
+embed = torch.randn((50, 512)).to(device)
+model = ArcFace(512, 10000, 30, 0.2, False).to(device)
 
 
-def speed_test_unit():
+def speed_test_unit1():
     model.forward(embed, label)
 
-def speed_test():
-    time_cost = timeit.repeat(stmt="speed_test_unit()", setup="from __main__ import speed_test_unit", number=100,
-                              repeat=1000)
-    print("time_cost = {} ms".format(min(time_cost) * 1000))
+
+def speed_test_unit2():
+    model.forward2(embed, label)
+
+
+def speed_test_unit3():
+    model.forward3(embed, label)
+
+
+def speed_test(repeat=100):
+    time_cost1 = timeit.repeat(stmt="speed_test_unit1()", setup="from __main__ import speed_test_unit1", number=100,
+                               repeat=repeat)
+    time_cost2 = timeit.repeat(stmt="speed_test_unit2()", setup="from __main__ import speed_test_unit2", number=100,
+                               repeat=repeat)
+    time_cost3 = timeit.repeat(stmt="speed_test_unit3()", setup="from __main__ import speed_test_unit3", number=100,
+                               repeat=repeat)
+    print("time_cost1 = {} ms\n"
+          "time_cost2 = {} ms\n"
+          "time_cost3 = {} ms".format(min(time_cost1) * 1000,
+                                      min(time_cost2) * 1000,
+                                      min(time_cost3) * 1000))
 
 
 def equal_test():
-    loss1 = model.forward1(embed, label)
-    loss2 = model.forward2(embed, label)
-    loss3 = model.forward3(embed, label)
+    with torch.set_grad_enabled(False):
+        loss1 = model.forward(embed, label)
+        loss2 = model.forward2(embed, label)
+        loss3 = model.forward3(embed, label)
     print("loss1: {}".format(loss1))
     print("loss2: {}".format(loss2))
     print("loss3: {}".format(loss3))
 
 
 if __name__ == '__main__':
-    speed_test()
+    speed_test(100)
     equal_test()
